f0ceb6209a5b69a92c020ff2dacf9ae9b500f900
Mrrvm <mrrvm@hotmail.com>
2025-04-07 16:23:38 +0200

    vault backup: 2025-04-07 16:23:38

--- quartz/components/CustomMeta.tsx
+++ quartz/components/CustomMeta.tsx
@@ -4,10 +4,9 @@ import { classNames } from "../util/lang"
 const CustomMeta: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
     return (
         <div class={classNames(displayClass, "custom-meta")}>
-            {fileData.frontmatter?.bib && <div class="meta">Bib: {fileData.frontmatter.bib}</div>}
+            {fileData.frontmatter?.bib && <div>{fileData.frontmatter.bib}</div>}
             {fileData.frontmatter?.link && (
                 <div class="meta">
-                    Link:{" "}
                     <a href={fileData.frontmatter.link.toString()} target="_blank" rel="noopener">
                         {fileData.frontmatter.link}
                     </a>
@@ -20,8 +19,11 @@ const CustomMeta: QuartzComponent = ({ fileData, displayClass }: QuartzComponent
 CustomMeta.css = `
 .custom-meta {
   margin-top: 0.5rem;
-  font-size: 0.875rem;
+  font-size: 1rem;
   color: var(--gray);
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
 }
 `
 
